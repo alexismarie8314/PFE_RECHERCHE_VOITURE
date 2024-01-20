10316b777632a4868745247ee60ccd5761fbb291
Heller-Theo <hellertheo8@gmail.com>
2024-01-20 17:49:33 +0100

    creation of datasets for the graph

--- opti/graph_paris.py
+++ opti/graph_paris.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 
 # Charger le fichier OSM
-tree = ET.parse('map_bischo_petit.osm')
+tree = ET.parse('map_lyon.osm')
 root = tree.getroot()
 
 # Créer un graphe dirigé avec networkx
 G = nx.DiGraph()
+
+# valeur par défaut de la vitesse
+default_speed = "default"
 
 # Parcourir les nœuds (nodes) du fichier OSM
 for node in root.findall(".//node"):
@@ -41,7 +44,11 @@ for way in root.findall(".//way"):
     
     # Ajouter la vitesse sur la voie
     speed_tag = way.find(".//tag[@k='maxspeed']")
-    speed = int(speed_tag.get('v').split()[0]) if speed_tag is not None else "default"  # La vitesse par défaut est 50 km/h
+    #speed = int(speed_tag.get('v').split()[0]) if speed_tag is not None else "default"  # La vitesse par défaut est 50 km/h
+    
+    speed_value = speed_tag.get('v').split()[0] if speed_tag is not None and speed_tag.get('v').split()[0].isdigit() else default_speed
+    speed = int(speed_value) if speed_value.isdigit() else default_speed  # La vitesse par défaut est 50 km/h
+
     
     way_id = way.get('id')
     nodes = [nd.get('ref') for nd in way.findall(".//nd")]
@@ -75,6 +82,25 @@ for node in nodes_to_remove:
     G.remove_node(node)
     G.add_edge(predecessors[0], successors[0], type=pred_edge_type, color=pred_edge_color, speed=pred_edge_speed)
 
+#supprimer les arrêtes qui bouclent sur elles-mêmes
+edges_to_remove = []
+for edge in G.edges():
+    if edge[0] == edge[1]:
+        edges_to_remove.append(edge)
+G.remove_edges_from(edges_to_remove)
+
+nodes_without_position = [node for node in G.nodes() if 'pos' not in G.nodes[node]]
+print(f"Nombre de noeuds sans position: {len(nodes_without_position)}")
+G.remove_nodes_from(nodes_without_position)
+print(f"Nombre de noeuds: {len(G.nodes())}")
+print(f"Nombre de routes: {len(G.edges())}")
+print(f"Nombre de routes à sens unique: {len([edge for edge in G.edges() if G[edge[0]][edge[1]]['type'] == 'oneway'])}")
+print(f"Nombre de routes à double sens: {len([edge for edge in G.edges() if G[edge[0]][edge[1]]['type'] == 'twoway'])}")
+print(f"Nombre de routes sans vitesse: {len([edge for edge in G.edges() if G[edge[0]][edge[1]]['speed'] == default_speed])}")
+print(f"Nombre de routes avec vitesse: {len([edge for edge in G.edges() if G[edge[0]][edge[1]]['speed'] != default_speed])}")
+print(f"Vitesse maximale: {max([G[edge[0]][edge[1]]['speed'] for edge in G.edges() if G[edge[0]][edge[1]]['speed'] != default_speed])} km/h")
+print(f"Vitesse minimale: {min([G[edge[0]][edge[1]]['speed'] for edge in G.edges() if G[edge[0]][edge[1]]['speed'] != default_speed])} km/h")
+print(f"Vitesse moyenne: {sum([G[edge[0]][edge[1]]['speed'] for edge in G.edges() if G[edge[0]][edge[1]]['speed'] != default_speed]) / len([edge for edge in G.edges() if G[edge[0]][edge[1]]['speed'] != default_speed])} km/h")
 
 # Dessiner le graphe avec les valeurs de vitesse maximale
 pos = nx.get_node_attributes(G, 'pos')
@@ -84,7 +110,29 @@ edge_colors = [G[edge[0]][edge[1]]['color'] for edge in G.edges()]
 edges_with_speed = [(edge[0], edge[1]) for edge in G.edges() if 'speed' in G[edge[0]][edge[1]]]
 edge_labels = {(edge[0], edge[1]): f"{G[edge[0]][edge[1]]['speed']} km/h" for edge in edges_with_speed}
 
-nx.draw(G, pos, with_labels=True, font_size=5, node_size=5, node_color='lightblue', edge_color=edge_colors, linewidths=0.1)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=5, alpha=0.7)
+# nx.draw(G, pos, with_labels=True, font_size=5, node_size=5, node_color='lightblue', edge_color=edge_colors, linewidths=0.1)
+# nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=5, alpha=0.7)
 
-plt.show()
+# plt.show()
+
+# exporter le graphe sous la forme d'une liste d'adjacence avec les attributs 'speed' et 'type' ainsi que les positions des noeuds et leur identifiant
+Node_list = []
+for node in G.nodes():
+    Node_list.append([node, G.nodes[node]['pos'][0], G.nodes[node]['pos'][1]])
+
+Edge_list = []
+for edge in G.edges():
+    #précise le sens de l'arête oneway
+    Edge_list.append([edge[0], edge[1], G[edge[0]][edge[1]]['speed'], G[edge[0]][edge[1]]['type']])
+    
+#convertir la liste en dataframe
+import pandas as pd
+df = pd.DataFrame(Node_list)
+df.columns = ['node_id', 'lon', 'lat']
+df.to_csv('Node_list.csv', index=False)
+
+df = pd.DataFrame(Edge_list)
+df.columns = ['node_id1', 'node_id2', 'speed', 'type']
+df.to_csv('Edge_list.csv', index=False)
+
+
--- opti/test.py
+++ opti/test.py
@@ -1,15 +1,12 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-import xml.etree.ElementTree as ET
 
-# ... (votre code existant)
 
-# Dessiner le graphe avec les valeurs de vitesse maximale
-pos = nx.get_node_attributes(G, 'pos')
-edge_colors = [G[edge[0]][edge[1]]['color'] for edge in G.edges()]
-edge_labels = {(edge[0], edge[1]): f"{G[edge[0]][edge[1]]['speed']} km/h" for edge in G.edges()}
+import numpy as np
 
-nx.draw(G, pos, with_labels=True, font_size=5, node_size=5, node_color='lightblue', edge_color=edge_colors, linewidths=0.1)
-nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=5, alpha=0.7)
 
-plt.show()
+# découpe un intervalle en 5 sous-intervalles
+A = np.linspace(4.7392, 4.9761, 6)
+B = np.linspace(45.8178, 45.6827, 6)
+
+
+print(A)
+print(B)

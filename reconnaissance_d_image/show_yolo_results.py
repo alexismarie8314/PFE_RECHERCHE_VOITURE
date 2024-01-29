1c23734193b669f657c35c30271c1ec657e0d6f8
Luca Bankofski <93180112+CariacouP@users.noreply.github.com>
2024-01-29 15:56:17 +0100

    show yolo results

--- reconnaissance_d_image/show_yolo_results.py
+++ reconnaissance_d_image/show_yolo_results.py
@@ -5,8 +5,7 @@ import cv2
 
 class YOLO_Results() : 
     def __init__(self, video_path="../Dataset/CarCrash/videos/Normal/000100.mp4",model = 'yolov8n.pt'):
-        
-        self.model=model = YOLO(model)
+        self.model = YOLO(model)
         self.cap = cv2.VideoCapture(video_path)
     
     def show_results(self):
